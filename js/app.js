8e6f8257d2b5ebf7016d2073afd200c30ed21b72
3otes <johnbotes@yahoo.com>
2018-10-10 11:45:21 +0200

    my image

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function (x, y) {
+let Enemy = function (x, y) {
   this.sprite = 'images/enemy-bug.png';
   this.x = x;
   this.y = y;
@@ -20,7 +20,7 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
+let Player = function() {
  this.sprite = 'images/char-boy.png';
  this.x = 200;
  this.y = 405;
@@ -64,14 +64,14 @@ Player.prototype.handleInput = function (dt) {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-  var allEnemies = [new Enemy(-200, 65), new Enemy(-150, 145), new Enemy(-100, 230)];
+// Place the player object in a letiable called player
+  let allEnemies = [new Enemy(-200, 65), new Enemy(-150, 145), new Enemy(-100, 230)];
 
-  var player = new Player();
+  let player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
